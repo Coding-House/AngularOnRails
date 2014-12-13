8752a9eca993f314c7ee667b9334f0c9a59aea67
CodingHouse <sb@codinghouse.co>
2014-12-13 13:10:13 -0800

    Create an answer endpoint

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -2,4 +2,12 @@ class AnswersController < ApplicationController
   def index
     render json: Answer.all
   end
+
+  def create
+    answer = Answer.create email: params[:email],
+      body: params[:body],
+      question_id: params[:question_id]
+    render json: answer
+  end
+
 end

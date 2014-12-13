c45f633b23887dac17a2f9dbcacdd3df3a0e878a
CodingHouse <sb@codinghouse.co>
2014-12-13 13:52:33 -0800

    Clean up the markup

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,7 +10,14 @@
 // Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require jquery
-//= require jquery_ujs
-//= require turbolinks
+
 //= require_tree .
+//= require_self
+
+console.log("Works!");
+
+var AoRApp = angular.module('AoR', [])
+ .controller('MainController',
+    function($scope) {
+      $scope.var1 = 'CodingHouse';
+  });
